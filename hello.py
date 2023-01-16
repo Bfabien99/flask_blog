6c1800937a3b90b4d0b04036d1349f59e41f1301
bfabien99 <fabienbrou99@gmail.com>
2023-01-16 10:28:45 +0000

    flask's make_response

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, make_response
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,6 +13,13 @@ def hello(name):
 def browser():
     user_agent = request.headers.get('User-Agent')
     return '<h1>Your browser is %s!</h1>' %user_agent
+
+@app.route('/cookie')
+def cookie():
+    cook = request.cookies.get('answer', None)
+    response = make_response(f'<h1>this page carrie a cookie! {cook}</h1>')
+    response.set_cookie('answer', '42')
+    return response
 
 if __name__=='__main__':
     app.run(debug=True)
