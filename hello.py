7a2cab4e7d6fd569251d0bf28d33695ddfed7c2f
bfabien99 <fabienbrou99@gmail.com>
2023-01-16 11:43:49 +0000

    discover flask_moment

--- hello.py
+++ hello.py
@@ -1,8 +1,12 @@
 from flask_bootstrap import Bootstrap5
+from flask_moment import Moment
 from flask import Flask, render_template
+
+from datetime import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
+moment = Moment(app)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -10,7 +14,7 @@ def page_not_found(e):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def hello(name):
