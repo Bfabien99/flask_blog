1e31ca550ac02385b0a5593a094287f61fa82424
bfabien99 <fabienbrou99@gmail.com>
2023-01-16 12:46:29 +0000

    work with flask wtf

--- hello.py
+++ hello.py
@@ -2,19 +2,34 @@ from flask_bootstrap import Bootstrap5
 from flask_moment import Moment
 from flask import Flask, render_template
 
+from wtforms import SubmitField, StringField
+from flask_wtf import FlaskForm
+
 from datetime import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
 moment = Moment(app)
+app.config['SECRET_KEY'] = 'bvuyhgvbkjhkj++'
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?')
+    submit = SubmitField('Submit')
+
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('Errors/404.html'), 404
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    names = None
+    form = NameForm()
+    if form.validate_on_submit():
+        print(form.name.data)
+        names = form.name.data
+        form.name.data = ''
+    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=names)
 
 @app.route('/user/<name>')
 def hello(name):
