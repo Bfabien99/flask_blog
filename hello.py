fcd31b7dbabdb4f38ed253bb2e18eb0f8f9c2d43
bfabien99 <fabienbrou99@gmail.com>
2023-01-16 10:51:43 +0000

    rendering template

--- hello.py
+++ hello.py
@@ -1,25 +1,14 @@
-from flask import Flask, request, make_response
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def hello(name):
-    return '<h1>Hello %s!</h1>' %name
+    return render_template('user.html', name=name)
 
-@app.route('/browser')
-def browser():
-    user_agent = request.headers.get('User-Agent')
-    return '<h1>Your browser is %s!</h1>' %user_agent
-
-@app.route('/cookie')
-def cookie():
-    cook = request.cookies.get('answer', None)
-    response = make_response(f'<h1>this page carrie a cookie! {cook}</h1>')
-    response.set_cookie('answer', '42')
-    return response
 
 if __name__=='__main__':
     app.run(debug=True)
